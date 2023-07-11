f98e0ae7ebf9b2393f2925c69700cd5e3317c74b
Bilostenko <bilostenkod@gmail.com>
2023-07-11 22:34:50 +0300

    router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,15 +4,30 @@ import './reset.css'
 
 import Header from '../header/Header';
 import Nav from '../nav/Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Profile from '../profile/Profile';
+import Dialogs from '../dialogs/Dialogs';
+import News from '../news/News';
+import Music from '../music/Music';
+import Settings from '../set/Settings';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Nav />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Nav />
+        <div className="content">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -4,7 +4,7 @@ import "./profile.css"
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div>
       <div>
         <img className='bcgrnd_image' src="https://media.istockphoto.com/id/1460655983/photo/a-ocean-waves-and-beach-top-view-natural-background.webp?b=1&s=170667a&w=0&k=20&c=SQW8xiYa4QH9d7s1R02u9wSrM6C7vjWCTcFzVEyZjaM=" alt="image" />
       </div>
