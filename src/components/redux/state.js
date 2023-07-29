c5467d415e9522034b72a8a225b19f304ea0ed0a
Bilostenko <bilostenkod@gmail.com>
2023-07-29 17:55:59 +0300

    store, state, ООП, рефакторинг

--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,67 +1,66 @@
-let rerenderEntireTree = () =>{
-  return console.log("state is changed")
-}
-
-let state = {
-  profilePage: {
-
-    postsData: [
-      { id: 1, post: "Hi, how are you?", like: 10 },
-      { id: 2, post: "It's my first message", like: 15 },
-    ]
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, post: "Hi, how are you?", like: 10 },
+        { id: 2, post: "It's my first message", like: 15 },
+      ]
+    },
+    messagesPage: {
+      dialogsData: [
+        { id: 1, name: "Dima" },
+        { id: 2, name: "Lena" },
+        { id: 3, name: "Nelson" },
+        { id: 4, name: "Chester" },
+      ],
+      messagesData: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "Yo" },
+        { id: 3, message: "How are you?" },
+        { id: 4, message: "Call me!" },
+      ]
+    },
+    friends: {
+      friendsData: [
+        { id: 1, name: "Віра" },
+        { id: 2, name: "Надія" },
+        { id: 3, name: "Ігнат" },
+      ],
+      friendsPhoto: [
+        { id: 1, photo: "https://i.redd.it/id910ovp3rm71.jpg" },
+        { id: 2, photo: "https://i.pinimg.com/280x280_RS/fa/aa/0e/faaa0ebf979da5e7d5cf038a5bcb7853.jpg" },
+        { id: 3, photo: "https://i0.wp.com/manforhimself.com/wp-content/uploads/2021/05/Ross-geller-swept-back-hair-1200-GettyImages-908307.jpg?fit=1200%2C1200&ssl=1" },
+      ]
+    }
   },
-  messagesPage: {
 
-    dialogsData: [
-      { id: 1, name: "Dima" },
-      { id: 2, name: "Lena" },
-      { id: 3, name: "Nelson" },
-      { id: 4, name: "Chester" },
-    ],
-    messagesData: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "Yo" },
-      { id: 3, message: "How are you?" },
-      { id: 4, message: "Call me!" },
-    ]
+  getState(){
+    return this._state
   },
-  friends: {
-    friendsData: [
-      { id: 1, name: "Віра" },
-      { id: 2, name: "Надія" },
-      { id: 3, name: "Ігнат" },
-    ],
-    friendsPhoto: [
-      { id: 1, photo: "https://i.redd.it/id910ovp3rm71.jpg" },
-      { id: 2, photo: "https://i.pinimg.com/280x280_RS/fa/aa/0e/faaa0ebf979da5e7d5cf038a5bcb7853.jpg" },
-      { id: 3, photo: "https://i0.wp.com/manforhimself.com/wp-content/uploads/2021/05/Ross-geller-swept-back-hair-1200-GettyImages-908307.jpg?fit=1200%2C1200&ssl=1" },
-
-    ]
+  rerenderEntireTree() {
+    console.log("state is changed");
+  },
+  addPost(postMessage) {
+    let newPost = {
+      id: this._state.profilePage.postsData.length + 1,
+      post: postMessage,
+      like: 0
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this.rerenderEntireTree(this._state);
+  },
+  addMessage(message) {
+    let newMessage = {
+      id: this._state.messagesPage.messagesData.length + 1,
+      message: message
+    };
+    this._state.messagesPage.messagesData.push(newMessage);
+    this.rerenderEntireTree(store._state);
+  },
+  subscribe(observer) {
+    this.rerenderEntireTree = observer;
   }
+};
 
-}
-
-export const addPost = (postMessage) => {
-  let newPost = {
-    id: state.profilePage.postsData.length + 1,
-    post: postMessage,
-    like: 0
-  }
-  state.profilePage.postsData.push(newPost);
-  rerenderEntireTree(state)
-}
-
-export const addMessage = (message) => {
-  let newMessage = {
-    id: state.messagesPage.messagesData.length+1,
-    message: message
-  }
-  state.messagesPage.messagesData.push(newMessage)
-  rerenderEntireTree(state)
-}
-
-export const subscribe = (observer) =>{
-  rerenderEntireTree = observer
-}
-
-export default state
+export default store;
+window.store = store;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import state, {subscribe} from './components/redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/App';
-import { addPost, addMessage } from "./components/redux/state";
-
-
+import store from './components/redux/state';
 
 export let rerenderEntireTree = (state) => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
-      <App appState={state} addPost={addPost} addMessage={addMessage}/>
+      <App appState={state} addPost={store.addPost.bind(store)} addMessage={store.addMessage.bind(store)} /> 
+      {/* .bind надо для привязки вд даном случае к store */}
     </React.StrictMode>
   );
 }
 
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
